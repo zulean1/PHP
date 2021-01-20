8a32eba319f1d4501d03544d49ce120cb55cac8f
zulean1 <zuca0107@gmail.com>
2021-01-19 22:53:19 -0400

    Cambios

--- POO/practica3/escuela.php
+++ POO/practica3/escuela.php
@@ -3,14 +3,20 @@
 
 class Escuela
 {
-
+    private $nombre;
+    private  $ubicacion;
 	
 	function __construct($ubicacion)
 	{
 	 
-
+       $this->ubicacion = $ubicacion;
 	  echo "La escuela esta ubicada en : $ubicacion";
 
+	}
+
+	function getCochesEscolares ($coches)
+	{
+		$coches =[];
 	}
 
 
